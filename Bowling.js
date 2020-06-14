6002e2435593205719a676511aa139f9e61937b0
Laura Bassani <bassanilaurarita@gmail.com>
2020-06-14 17:26:13 +0200

    implement strikes

--- Bowling.js
+++ Bowling.js
@@ -8,36 +8,51 @@ class Bowling {
     }
 
     score() {
-        let counter = 0;
+        let total = 0;
         for (let i = 0; i < this.turns.length; i += 2) {
             let firstTurn = this.turns[i];
             let secondTurn = this.turns[i + 1];
             let thirdTurn = this.turns[i + 2];
-            counter += this.frame(firstTurn, secondTurn, thirdTurn);
+            let fourthTurn = this.turns[i + 3];
+            total += this.frame(firstTurn, secondTurn, thirdTurn, fourthTurn);
         }
-        return counter;
+        return total;
     }
 
     roll(n) {
-        this.countRolls++;
         this.turns.push(n);
+        if (this.countRolls % 2 === 0 && this.isStrike(n)) {
+            this.turns.push(0)
+        }
+        this.countRolls++;
+
     }
 
     isGameOver() {
         return this.countRolls >= 20
     }
 
-    frame(a = 0, b = 0, c = 0) {
-        if (this.isSpare(a, b)) {
-            return 10 + c
+    frame(firstTurn = 0, secondTurn = 0, thirdTurn = 0, fourthTurn = 0) {
+
+        if (this.isStrike(firstTurn)) {
+            return 10 + thirdTurn + fourthTurn;
+        } else if (this.isSpare(firstTurn, secondTurn)) {
+            return 10 + thirdTurn
         } else {
-            return a + b;
+            return firstTurn + secondTurn;
         }
     }
 
-    isSpare(a, b) {
-        return a + b === 10;
+    isStrike(firstTurn) {
+        return firstTurn === 10;
     }
+
+    isSpare(firstTurn, secondTurn) {
+        return firstTurn + secondTurn === 10;
+    }
+
+
+
 }
 
 
--- Bowling.test.js
+++ Bowling.test.js
@@ -1,34 +1,44 @@
 const Bowling = require("./Bowling")
 
-test("score starts at 0", () => {
-    let bowling = new Bowling()
-    expect(bowling.score()).toBe(0)
-})
+describe('bowling', function () {
 
-test("roll updates score", () => {
-    let bowling = new Bowling()
-    bowling.roll(1)
-    expect(bowling.score()).toBe(1)
-    bowling.roll(5)
-    expect(bowling.score()).toBe(6)
-})
+    test("score starts at 0", () => {
+        let bowling = new Bowling()
+        expect(bowling.score()).toBe(0)
+    })
 
-test("is game over?", () => {
-    let bowling = new Bowling()
-    expect(bowling.isGameOver()).toBeFalsy()
-    for (let i = 0; i < 20; i++) {
+    test("roll updates score", () => {
+        let bowling = new Bowling()
         bowling.roll(1)
-    }
-    expect(bowling.isGameOver()).toBeTruthy()
-})
+        expect(bowling.score()).toBe(1)
+        bowling.roll(5)
+        expect(bowling.score()).toBe(6)
+    })
 
-test("is this a spare?", () => {
-    let bowling = new Bowling()
-    bowling.roll(1)
-    bowling.roll(9)
-    expect(bowling.score()).toBe(10)
-    bowling.roll(4)
-    expect(bowling.score()).toBe(18)
-})
+    test("is game over?", () => {
+        let bowling = new Bowling()
+        expect(bowling.isGameOver()).toBeFalsy()
+        for (let i = 0; i < 20; i++) {
+            bowling.roll(1)
+        }
+        expect(bowling.isGameOver()).toBeTruthy()
+    })
 
+    test("is this a spare?", () => {
+        let bowling = new Bowling()
+        bowling.roll(1)
+        bowling.roll(9)
+        expect(bowling.score()).toBe(10)
+        bowling.roll(4)
+        expect(bowling.score()).toBe(18)
+    })
 
+    test("is this a strike?", () => {
+        let bowling = new Bowling()
+        bowling.roll(10)
+        bowling.roll(5)
+        bowling.roll(2)
+        expect(bowling.score()).toBe(24)
+    })
+
+});
